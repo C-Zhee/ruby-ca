2deffa1062d116104277f04a9a35e4da08cef8a3
Corey <coreyzhee@gmail.com>
2023-03-19 23:56:42 -0400

    arrays

---
+++ arrays-and-hashes-a/1.rb
@@ -0,0 +1,15 @@
+# Creating Arrays
+
+girls = ["Rochelle", "Amelie", "Angela"]
+puts girls
+
+# Access by Index
+
+puts girls[1]   #Amelie
+
+# Arrays of Non-Numbers
+
+clothing = ["shoes", "coats", "pants"]
+# puts clothing
+
+# Arrays of Arrays
