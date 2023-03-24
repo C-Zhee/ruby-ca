29eeeeac9f1dc0c7c15ba7158999dd0f895eb8bf
Corey <coreyzhee@gmail.com>
2023-03-23 23:40:42 -0400

    lazy day

---
+++ blocks-and-sorting-a/1.rb
@@ -0,0 +1,34 @@
+# Methods
+# Methods are a resuable section of code written to perform a 
+# specific task in a program
+
+# Method Syntax
+
+def welcome
+    print "Welcome to Ruby!"
+end
+
+# welcome
+# puts welcome
+
+# Create Your Own
+
+def lemonz
+    puts "Happy Birthday!"
+end
+
+# Calling a function
+
+# lemonz
+# puts lemonz
+
+# Parameters and Arguments
+
+def lemonz(param)
+    puts param ** 3
+end
+
+lemonz(3)                     # 3 squared is 27
+
+# Splat!
+
