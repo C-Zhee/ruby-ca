b48bd638bb993a8d728c5c3f34d700e6dc5a1af6
Corey <coreyzhee@gmail.com>
2023-03-25 23:29:12 -0400

    hashes-a

--- hashes-and-symbols-a/2.rb
+++ hashes-and-symbols-a/2.rb
@@ -1,3 +1,14 @@
+# Converting Between Symbols and Strings
+
+sports = ["Basketball", "Baseball", "Foozball", "Tennis"]
+
+symbols = []
+
+sports.each do |sports|
+    symbols.push(sports.to_sym)
+end
+print symbols
+
 # Many Paths to the Same Summit
 
 # .to_sym is the same as .intern
@@ -12,3 +23,33 @@ hash_with_symbols = {
 
 puts hash_with_symbols
 
+# The Hash Rocket Has Landed
+
+gorls = { :Lemonz => "Gorgeous",
+:Limez => "Cutie"}
+puts gorls
+
+# This way is the 1.9 version
+gorls = { Lemonz: "Gorgeous",
+Limez: "Cutie"}
+puts gorls
+
+# Dare to Compare
+
+# Shows a block of code that proves that symbol keys are faster then string keys
+
+# Becoming More Selective
+
+soda_hash = { dr_pepper: 10,
+pepsi: 9,
+coke: 8,
+fanta: 8}
+
+rating_soda = soda_hash.select { |soda, rating| rating > 8 }
+
+puts rating_soda
+
+# More Methods, More Solutions
+
+soda_hash.each { |x,y| puts x}
+soda_hash.each_key { |x,y| puts x}
