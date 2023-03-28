1f4d7863e3a5aa214ce93a936add462e2063262b
Corey <coreyzhee@gmail.com>
2023-03-28 04:26:27 -0400

    blocks-procs

--- blocks-procs-and-lambdas/1.rb
+++ blocks-procs-and-lambdas/1.rb
@@ -10,5 +10,32 @@ number_array = [10, 20, 30]
 number_array.collect { |number| puts number ** 2}
 
 # Learning to Yield
+# yeild lets us add code into the block
+
+# Yieling with Parameters
+
+def yield_name(name)
+    puts "let's do a yield!"
+        yield("Kim")
+        puts "in-between the yield!"
+        yield (name)
+        puts "block complete"
+    end
+
+    yield_name("Olivia") { |n| puts "My name is #{n}!"}
+
+# Try it Yourself!
+
+def triple(number)
+    yield number
+end
+
+triple(10) { |num| puts num * 3 }
+
+# Keeping Your Code DRY
+
+# Proc
+# a proc is a Ruby object which can be stored in a variable and therefore 
+# reused many times throughout a program
 
 
