07034922372ba0bac9b7895532f6255f14161b3e
Corey <coreyzhee@gmail.com>
2023-03-30 00:52:11 -0400

    object-oritented-yes

--- object-oriented-2-a/2.rb
+++ object-oriented-2-a/2.rb
@@ -2,3 +2,53 @@
 # Modules are like classes! Except they can't create instances or have subclasses. They can only store information
 
 # Module Syntax
+
+module Leemoonz
+    LEMONZ = "BESTIE"
+end
+
+# Resolve to Keep Learning
+
+puts Leemoonz::LEMONZ
+
+# A Few Requirements
+
+require 'date'
+require 'time'
+
+puts Date.today
+puts Time.now
+
+# Feeling Included
+# include    When we use this, we can include the module's methods
+
+# The Marriage of Modules and Classes
+
+module Action
+  def jump
+    @distance = rand(4) + 2
+    puts "I jumped forward #{@distance} feet!"
+  end
+end
+
+class Rabbit
+  include Action
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Cricket
+  include Action
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+peter = Rabbit.new("Peter")
+jiminy = Cricket.new("Jiminy")
+
+peter.jump
+jiminy.jump
---
+++ object-oriented-2-a/3.rb
@@ -0,0 +1,2 @@
+# Imitating Multiple Inheritance
+
